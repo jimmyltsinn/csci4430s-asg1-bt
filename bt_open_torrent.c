0adea918598c2ff83d5182e195ba8466c1a2caf3
Jimmy Sinn <jimmysinn91@gmail.com>
2012-02-26 16:44:04 +0800

    Fix endianess problem on opening torrent file

--- bt_open_torrent.c
+++ bt_open_torrent.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <endian.h>
+#include <stdint.h>
 
 int torrent_file(char *filename) {
     int fd;
-    int id, size;
+
+    unsigned int id, size;
     char tip[4];
-    short tport;
+    unsigned short tport;
     char *name;
 
     fd = open(filename, O_RDONLY);
@@ -20,14 +23,16 @@ int torrent_file(char *filename) {
     read(fd, name, size);
     name[size] = '\0';
     
+    *tip = le32toh((uint32_t) *tip);
+
     read(fd, &size, 4);
     close(fd);
 
-    printf("ID = %d\n", id);
+    printf("ID = %x\n", id);
     printf("Tracker IP: %d.%d.%d.%d\n", tip[0], tip[1], tip[2], tip[3]);
     printf("Tracker Port: %d\n", tport);
     printf("File name: %s\n", name);
-    printf("File size: %d\n", size);
+    printf("File size: %u\n", size);
     return 0;
 }
 
